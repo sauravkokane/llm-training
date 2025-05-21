974a717e1463e81ba670562b89a6c3786693d9f2
theSK2001 <sbkok6470@gmail.com>
2025-05-21 21:37:29 +0530

    Changed in model name

--- 1. models/Chat-Models/chat_models_openai_demo.py
+++ 1. models/Chat-Models/chat_models_openai_demo.py
@@ -2,7 +2,7 @@ from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 load_dotenv()
 
-model = ChatOpenAI(model='gpt-4')
+model = ChatOpenAI(model='gpt-4', max_tokens=200, temperature=0.7)
 
 result = model.invoke("what is best in Mumbai?", )
 print(result)
