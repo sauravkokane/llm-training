20a1dc0ef1f7df29b77df0fc8a2e07f861efed8d
theSK2001 <sbkok6470@gmail.com>
2025-05-29 09:47:02 +0530

    learned about text loaders

---
+++ 7. document_loaders/loader_text.py
@@ -0,0 +1,25 @@
+from langchain_community.document_loaders import TextLoader
+
+loader = TextLoader(file_path="example.txt", encoding='utf-8')
+
+docs = loader.load()
+
+print(f"Type of Docs: {type(docs)}") # <class 'list'>
+print(f"length of Docs: {len(docs)}") # 1
+print(f"Type of Doc: {type(docs[0])}") # <class 'langchain_core.documents.base.Document'>
+print(f"First Doc: {docs[0]}")
+
+
+# [Document(
+# metadata={'source': 'example.txt'}, 
+# page_content='''Memory management is always  an extremely important part of any program or application being developed in the Java
+# programming language. Whenever objects are created for classes in any programs or applications developed using Java language,
+# they are allocated a certain amount of memory allowing the program or application to function the way that it should.
+# \tJust like objects in Java, variables and objects need to be assigned or allocated certain areas of memory to ensure that they can be used
+# without errors or exceptions manifesting themselves. However, the memory that is allocated to them differs according to the
+# scope that the said variable is located in.
+# Once the memory allocated to a certain variable or object is deemed useless and the purpose of the variable or object has
+# been completed, it is important for the allocated memory to be recycled and be usable for other purposes. And that is where
+# the garbage collector in Java comes into play.
+# Fortunately for the Java programming language, memory reclamation is automatic in the Java vitual machine
+# (JVM), which means that Java developers do not necessarily have to go out of their way in order to free memory objects that''')]
